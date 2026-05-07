4f54b79b17a7891ee1f9c47dc0b2d5d5ff1c57f4
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2026-05-07 15:08:29 +0200

    first commit

---
+++ API/graphics/include/draw.h
@@ -0,0 +1,8 @@
+//
+// Created by Kalle on 07.05.2026.
+//
+
+#ifndef CITYBUILDER_DRAW_H
+#define CITYBUILDER_DRAW_H
+
+#endif  // CITYBUILDER_DRAW_H
---
+++ API/graphics/src/draw.cc
@@ -0,0 +1,5 @@
+//
+// Created by Kalle on 07.05.2026.
+//
+
+#include "draw.h"
